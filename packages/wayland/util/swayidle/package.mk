27b3a607f2cd43dcaf55ef0601ddc3b8d1435250
William Brawner <me@wbrawner.com>
2024-05-07 22:33:58 -0600

    Add swayidle as dependency for swaywm-env

---
+++ packages/wayland/util/swayidle/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
+
+PKG_NAME="swayidle"
+PKG_VERSION="1.8.0"
+PKG_SHA256="0fba74c520a2bd64acd00bc3bce7bc8c7b84a2609c0f66329d72dfb33cca03d7"
+PKG_LICENSE="MIT"
+PKG_SITE="https://swaywm.org/"
+PKG_URL="https://github.com/swaywm/swayidle/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain wayland wayland-protocols"
+PKG_LONGDESC="Idle management daemon for Wayland"
